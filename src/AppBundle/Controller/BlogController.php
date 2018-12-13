d761f2c53235ef7f47b7118f1ce61bf0482a4f3d
Harkame <louisr.daviaud@gmail.com>
2018-12-13 17:17:24 +0100

    dev bd

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -5,18 +5,34 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Symfony\Component\HttpFoundation\Response;
+
+use AppBundle\Entity\Post;
+
 class BlogController extends Controller
 {
     /**
      * @Route("/post/{article}", requirements={"article"="\d+"}, defaults={"article": "42"})
      */
-    public function postAction($article)
+    public function postAction(Post $article)
     {
-        dump($article);
+        $this->createAction();
+
+        dump(article);
 
         return $this->render('default/post.html.twig', array(
             'article' => $article
         ));
     }
 
+    public function createAction()
+    {
+        $post = Post();
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+
+        return new Response('Id du post');
+    }
 }
