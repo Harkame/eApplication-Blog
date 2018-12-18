b08ab923d8d5429a9c6d99174c3c49460a650242
skreo756 <bvolle766@gmail.com>
2018-12-18 18:22:59 +0100

    routes + tri date

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ https://devcenter.heroku.com/articles/heroku-postgresql#provisioning
 heroku addons:create heroku-postgresql:hobby-dev --version=9.5
 
 php5.6-sqlite3
+
+INSERT INTO POST (title, aliasUrl,Content, Published) VALUES (
+                                      "Mon premier article",
+                                      "article1", 
+                                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non urna ut ipsum rutrum porttitor. Nunc vitae feugiat ipsum. Vestibulum non faucibus mi. Duis ac leo sed libero placerat consequat. Mauris nec nisl imperdiet, fringilla nulla vel, tincidunt erat. Fusce bibendum sagittis lacinia. Vestibulum et suscipit purus, quis fermentum nulla. In commodo ante a metus facilisis, vitae tempus leo gravida. Vivamus sed arcu accumsan, fringilla mi ut, interdum nisl. Praesent malesuada at dui sit amet pulvinar. Sed iaculis tellus eu metus egestas efficitur.",
+                                      "2018-12-18 16:17:16"
+                                      )
+        
+
+             
+            
+
--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -14,24 +14,33 @@ class BlogController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function getAllPost()
+    public function indexAction()
     {
-        $posts = $this->getDoctrine()->getRepository('AppBundle:Post')->findAll();
+        $nbPosts = 10;
 
 
-        return $this->render('default/index.html.twig', [
+
+
+            $posts = $this->getDoctrine()->getRepository('AppBundle:Post')->findBy([], ['published' => 'DESC']);
+
+
+
+        return $this->render('default/index.html.twig', array('posts' => $posts,
+            'nbPosts' => $nbPosts));
+
+        /*    [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        ]); */
     }
 
     /**
-     * @Route("/post/{article}", requirements={"article"="\d+"}, defaults={"article": "42"})
+     * @Route("/post/{url_alias}" )
      */
-    public function getPost($article)
+    public function PostAction(string $url_alias)
     {
         $this->createAction();
 
-        $post = $this->getDoctrine()->getRepository('AppBundle:Post')->find($article);
+        $post = $this->getDoctrine()->getRepository('AppBundle:Post')->findBy(array('aliasUrl' => $url_alias));
 
         if(!$post)
             return $this->render('default/post.html.twig', array(
@@ -39,9 +48,14 @@ class BlogController extends Controller
             ));
         else
             return $this->render('default/post.html.twig', array(
-                'article' => var_dump($post)
+                'article' => $post
             ));
     }
+
+
+
+
+
 
     public function createAction()
     {
@@ -49,7 +63,9 @@ class BlogController extends Controller
 
         $post->setTitle('toto');
         $post->setAliasUrl('titi');
-        $post->setContent('tototo');
+        $post->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mauris lacus, ornare et massa a, posuere molestie dui. Curabitur sollicitudin, ex nec sollicitudin pretium, massa tellus vehicula velit, et vehicula sem orci nec felis. Curabitur ac vehicula justo, et pharetra mi. Suspendisse venenatis purus at leo egestas, a suscipit nisi aliquet. In laoreet nunc eget lectus aliquam, in mattis justo viverra. Pellentesque porta aliquet massa et finibus. Curabitur aliquam mauris vel neque maximus, at pretium enim commodo. Donec imperdiet lacinia ultrices. Fusce elementum at ante id laoreet. Nulla non nisl at elit porttitor mattis vel eu nisi. Fusce id congue quam. Morbi vitae interdum risus, dignissim tincidunt tortor. Etiam ex quam, condimentum vel lectus sit amet, interdum accumsan neque. Aenean at congue lacus, id commodo tortor.
+
+Ut quis odio lacus. Maecenas eget nulla a arcu feugiat porttitor a nec nulla. Proin luctus varius dui ut eleifend. Sed tincidunt elementum bibendum. Sed semper nunc eget massa elementum molestie. Phasellus posuere vulputate dui, a consectetur ante egestas ut. Sed euismod lacus bibendum elit tempor, a imperdiet nisl egestas. Etiam dignissim posuere tristique. Nulla sed sagittis felis, vitae maximus felis. Praesent pellentesque urna vel ex luctus laoreet. Nulla nulla elit, gravida sit amet consequat id, tincidunt in felis. Ut condimentum tellus enim, sed laoreet est pretium eu.');
         $post->setPublished( new \DateTime());
 
         $em = $this->getDoctrine()->getManager();
