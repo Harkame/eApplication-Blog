d86986b6d5ed6a00604fd3868e86742a0bf2ab0f
harkame <louisr.daviaud@gmail.com>
2019-01-01 05:18:51 -0800

    remove image

--- script/database_prod.sh
+++ script/database_prod.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+heroku restart --app louis-daviaud-blog
+
 heroku pg:reset DATABASE --app louis-daviaud-blog --confirm louis-daviaud-blog
 
 heroku run php bin/console doctrine:schema:create -a louis-daviaud-blog
 
 heroku run php bin/console doctrine:schema:update -a louis-daviaud-blog
+
+heroku restart --app louis-daviaud-blog
